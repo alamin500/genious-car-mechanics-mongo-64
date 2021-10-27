08cafbca8866ae605c9a027fc065c89f4e029894
alamin500 <nalamin500@gmail.com>
2021-10-27 18:57:12 +0600

    h

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,11 +3,14 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import "./AddService.css";
 const AddService = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
     console.log(data);
     axios.post("http://localhost:5000/services", data).then((res) => {
-      console.log(res);
+      if (res.data.insertedId) {
+        alert("added successfully");
+        reset();
+      }
     });
   };
   return (
